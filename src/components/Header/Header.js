6cdf1dc33753c4f15818be0a9f3109167ffe0830
Surendra Choudary Nettam <nettem.surendra@gmail.com>
2021-06-10 21:02:19 +0930

    final changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,27 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 
 export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a className="navbar-brand" href="/portfolio-react">Surendra C Nettam</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto mt-lg-0">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="/
-                        ">Home<span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link hover" to="/portfolio">Portfolio</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link hover" to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+        <a className="navbar-brand" href="/react-portfolio">Surendra C Nettam</a>
+          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="/react-portfolio">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/portfolio">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
     );
 }
